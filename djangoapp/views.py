f2af670029485e95dd7ecfd29d96b02dcf9b58b8
Crazyduderik <erik92perez92@gmail.com>
2024-10-13 17:51:24 -0700

    python search function

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -9,7 +9,34 @@ def index(request):
     return render(request, 'index.html')
 
 def search(request):
-    return render(request, 'djangoapp/search.html')
+    # GET search query adn filters
+    query = request.GET.get('query', '')
+    location = request.GET.get('location', '')
+    party = request.GET.get('party', '')
+    experience = request.GET.get('experience', '')
+    topics = request.GET.get('topics', '')
+    
+    # candidates
+    candidates = Candidate.objects.select_related('official_user')
+    
+    # apply filters
+    if query:
+        candidates = candidates.filter(official_user_firstname_icontains=query)
+        
+    if location:
+        candidates = candidates.filter(official_state_iexact=location)
+        
+    if party: 
+        candidates = candidates.filter(official_user_political_party_name_iexact=party)
+    
+    # Need to figure out how to filter experience
+    if experience:
+        pass
+    
+    if topics:
+        candidates = candidates.filter(policies_name_icontains=topics)
+        
+    return render(request, 'djangoapp/results.html',{'results' : candidates})
 
 def results(request):
     #results = User.objects.all()  # Query all users
