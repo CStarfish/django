4424ea126528c7e649c453dc3c917c26a63d182b
Jerry Ly <LyJeremy415@gmail.com>
2024-10-15 16:22:17 -0700

    Turning debug mode off

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2wqt=g^l2k268nnrgcoo!bg=i442s@*f%2xz84cbb@)4xb_mv%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
 ALLOWED_HOSTS = ['50.18.54.5']
                 #'127.0.0.1'] #for testing locally
