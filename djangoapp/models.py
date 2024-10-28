8c9eb66de6ceb5df123700e3dee325982a591132
Crazyduderik <erik92perez92@gmail.com>
2024-10-27 20:23:20 -0700

    dispay official user name on policy

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -199,7 +199,7 @@ class Candidate(models.Model):
         db_table = 'Candidate'
 
     def __str__(self):
-        return f"Candidate: {self.official}"
+        return f"Candidate: {self.user.user.first_name} {self.user.user.last_name}"#f"Candidate: {self.official}"
 
 
 class Policy(models.Model):
