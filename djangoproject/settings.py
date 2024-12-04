e4c234e86cc24c69c26cb657a5bf2694ed297766
bilguungzt <54411261+bilguungzt@users.noreply.github.com>
2024-12-03 16:59:18 -0800

    added profile_page, results, profile, password_changes, forgot_password styles

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2wqt=g^l2k268nnrgcoo!bg=i442s@*f%2xz84cbb@)4xb_mv%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['13.56.63.178']
-#'127.0.0.1'] #for testing locally
-
+ALLOWED_HOSTS = ['127.0.0.1']  # for testing locally
+# ['13.56.63.178']
 # Application definition
 
 INSTALLED_APPS = [
@@ -80,12 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'website',
-        'USER': 'jly',
-        #'USER': 'root',  # for local
-        'PASSWORD': 'CSC648@Server',
-        #'PASSWORD': '1234',  # for local
-        'HOST': '127.0.0.1',
-        #'HOST': 'localhost',  # for local
+        # 'USER': 'jly',
+        'USER': 'root',  # for local
+        # 'PASSWORD': 'CSC648@Server',
+        'PASSWORD': 'ComplexPass@123',  # for local
+        # 'HOST': '127.0.0.1',
+        'HOST': 'localhost',  # for local
         'PORT': '3306',  # Usually 3306
     }
 }
