8243dca1fd65babdaef4ab42f20e8c75366f6982
CStarfish <LyJeremy415@gmail.com>
2024-12-06 01:51:59 -0800

    Added new ip

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-2wqt=g^l2k268nnrgcoo!bg=i442s@*f%2xz84cbb@)4xb_mv%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']  # for testing locally
-# ['13.56.63.178']
+ALLOWED_HOSTS = [#'127.0.0.1']  # for testing locally
+'204.236.170.247']
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,12 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'website',
-        # 'USER': 'jly',
-        'USER': 'root',  # for local
-        # 'PASSWORD': 'CSC648@Server',
-        'PASSWORD': 'ComplexPass@123',  # for local
-        # 'HOST': '127.0.0.1',
-        'HOST': 'localhost',  # for local
+         'USER': 'jly',
+        #'USER': 'root',  # for local
+        'PASSWORD': 'CSC648@Server',
+        #'PASSWORD': 'ComplexPass@123',  # for local
+        'HOST': '127.0.0.1',
+        #'HOST': 'localhost',  # for local
         'PORT': '3306',  # Usually 3306
     }
 }
